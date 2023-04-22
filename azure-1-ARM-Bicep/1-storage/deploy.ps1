eed48b2a0ed1dfb00f2e959beffc6226b2de44e7
Espenk <espenkleivane@gmail.com>
2023-04-22 20:27:06 +0200

    init

--- azure-1-ARM-Bicep/1-storage/deploy.ps1
+++ azure-1-ARM-Bicep/1-storage/deploy.ps1
@@ -3,27 +3,27 @@
 Function LogResult($txt) {
     Add-Content log.txt $txt
 }
-$st = "Start-" + (Get-Date)
+$st = "Start deploy:" + (Get-Date)
 LogResult($st)
 
 # rg and location
 $rgName = "Rg-iac-0001"
 $location  = "uk south"
 
-# deployment name
-$deploymentUnixTime = Get-Date -UFormat %s
-$deploymentName = $deploymentUnixTime + "-unixTimeId"
-Write-Host $deploymentName
-LogResult($deploymentName)
+# deployment id
+$tempId = New-Guid
+$deploymentId = $tempId.ToString()
+Write-Host $deploymentId
+LogResult($deploymentId)
 
 # deploy rg
 New-AzResourceGroup -Name $rgName  -Location $location -Tag @{Infrastructure="IAC"} -Force
 
 # deploy resources
-$deployResult = New-AzResourceGroupDeployment -ResourceGroupName $rgName -Name $deploymentName -TemplateFile templates\main.bicep # -WhatIf
+$deployResult = New-AzResourceGroupDeployment -ResourceGroupName $rgName -Name $deploymentId -TemplateFile templates\main.bicep # -WhatIf
 
 Write-Host $deployResult.ProvisioningState
-$end = "End-" + ($deployResult.ProvisioningState)
+$end = "End deploy:" + ($deployResult.ProvisioningState)
 LogResult($end)
 
 
--- azure-1-ARM-Bicep/1-storage/remove.ps1
+++ azure-1-ARM-Bicep/1-storage/remove.ps1
@@ -1,3 +1,2 @@
-
 $rgName = "Rg-iac-0001"
 Remove-AzResourceGroup -Name $rgName -Force -AsJob
