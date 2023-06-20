8c7bd2a3e24e5dd17515813982d41f5e89a9d27b
Espenk <espenkleivane@gmail.com>
2023-06-20 12:05:53 +0200

    adminUsername

--- azure-extra-linux-vm/deploy.sh
+++ azure-extra-linux-vm/deploy.sh
@@ -17,6 +17,6 @@ echo ${myArray[1]}
 az config set bicep.use_binary_from_path=False
 
 az group create --location $location --name $resourceGroup --tags $tags
-az deployment group create --name mainDep --resource-group $resourceGroup --template-file main.bicep
+az deployment group create --name mainDep --resource-group $resourceGroup --template-file main.bicep --parameters adminUsername=$myArray[0]
 
 
