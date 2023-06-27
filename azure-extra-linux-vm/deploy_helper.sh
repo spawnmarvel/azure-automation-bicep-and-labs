b91aead94f0d1cdebb9de26c577cc44514d9d1fd
Espenk <espenkleivane@gmail.com>
2023-06-27 22:23:32 +0200

    helper

--- azure-extra-linux-vm/deploy_helper.sh
+++ azure-extra-linux-vm/deploy_helper.sh
@@ -1,21 +1,23 @@
+# Enter 
 az login --tenant
+#Deploy
 bash deploy.sh
-
-
+#vars
 rgName=''
 vmName=''
-
+#
 echo $rgName
+# list all
 az resource list --resource-group $rgName
 az resource list --resource-group $rgName --query [].name
 
-# Get IP address
+# get IP address
 az vm list-ip-addresses --resource-group $rgName --name $vmName --output table
-
+# n and p
 uName=""
-newPass=""
-
+newPass="" 
+# You must iterate this or change it before running below
 az vm user update --resource-group $rgName --name $vmName --username $uName --password $newPass
-
+# enter it
 ssh user@ipaddress
 
