7df85e1756f194cec716391af76192d5f8aa30d2
Espenk <espenkleivane@gmail.com>
2023-10-01 22:12:47 +0200

    secure maria

--- azure-extra-linux-vm/zabbix/install_mariadb.sh
+++ azure-extra-linux-vm/zabbix/install_mariadb.sh
@@ -13,16 +13,28 @@ sudo systemctl enable --now mariadb
 # https://stackoverflow.com/questions/24270733/automate-mysql-secure-installation-with-echo-command-via-a-shell-script
 
 # Make sure that NOBODY can access the server without a password
-sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
+# sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
 # Kill the anonymous users
-sudo mysql -e "DROP USER ''@'localhost'"
+# sudo mysql -e "DROP USER ''@'localhost'"
 # Because our hostname varies we'll use some Bash magic here.
-sudo omysql -e "DROP USER ''@'$(hostname)'"
+# sudo mysql -e "DROP USER ''@'$(hostname)'"
 # Kill off the demo database
-sudo mysql -e "DROP DATABASE test"
+# sudo mysql -e "DROP DATABASE test"
 # Make our changes take effect
-sudo mysql -e "FLUSH PRIVILEGES"
+# sudo mysql -e "FLUSH PRIVILEGES"
 # Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
+sudo mysql_secure_installation <<EOF
+
+n
+n
+n
+y
+y
+y
+y
+EOF
+
+
 echo "Mariadb successfully created!"
 
 # remove
