98110c029e7b8cb065b0e96c04babd5578ad9cd6
Espenk <espenkleivane@gmail.com>
2025-04-18 16:25:24 +0200

    init

--- azure-extra-linux-vm/test/deploy.sh
+++
@@ -1,3 +0,0 @@
-az group create --name exampleRG --location uksouth
-
-az deployment group create --resource-group exampleRG --template-file main.bicep
--- powershell-az-snippets/module/run.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module .\powershell-snippets\custom_module.psm1
-
-Add-LogOut("Log1")
-Add-ErrorOut("Error1")
-Add-Logger("Logger")
